c77d019b59ab74814dca740f264098a9b47e234b
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-18 05:00:55 +0600

    card section added without the scroll animation

--- app/page.js
+++ app/page.js
@@ -1,13 +1,20 @@
 "use client"
 import Header from '@/Components/Header/Header'
 import SectionLayout from '@/Components/SectionLayout/SectionLayout'
-import Image from 'next/image'
 import { styled } from 'styled-components'
-
+import { cards } from '@/utils/cards'
+import Card from '@/Components/Cards/Card'
 
 const MainStyled = styled.main`
   min-height: 100vh;
   width: 100vw;
+
+  .cards{
+    display: grid;
+    grid-template-columns: repeat(5, 17rem);
+    gap: 4rem;
+    posiition: absolute;
+  }
 `
 
 export default function Home() {
@@ -16,7 +23,14 @@ export default function Home() {
       <Header />
       <MainStyled>
         <SectionLayout>
+          <div className='cards'>
+            {cards.map(cardInfo => (<Card
+              key={cardInfo.id}
+              {...cardInfo}
+            >
 
+            </Card>))}
+          </div>
         </SectionLayout>
       </MainStyled>
 
